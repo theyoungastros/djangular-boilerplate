c06c379d150ef8532f80599f8c2429903597783a
Tyler D. Savery <tyler@bsideapp.com>
2015-09-12 13:24:18 -0400

    Updated manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "priceguess.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boiler.settings")
 
     from django.core.management import execute_from_command_line
 
